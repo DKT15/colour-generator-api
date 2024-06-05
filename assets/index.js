f8ead3b7e22457d9511ce13ab90eb465c6fee0cb
Danté Kellman-Thompson <dante.kellman@icloud.com>
2024-06-05 20:41:00 +0100

    added to the code.

---
+++ assets/index.js
@@ -0,0 +1,5 @@
+// Get Options for select in HTML and RGB capabilities from the API
+
+//Send users RGB/colour choice as well as option to API
+
+//Based on what has been set to the API should then be rendered to the page
